41fbb7e5ca2906cf810a9fa5245b99922249afcf
rafa906 <rafaibrahim1234@gmail.com>
2023-04-26 16:06:07 -0700

    string1.c

--- string1.c
+++ string1.c
@@ -23,7 +23,7 @@ char *_strcpy(char *dest, char *src)
 }
 
 /**
- * _strdup - duplicate a string
+ * _strdup - duplicates a string
  * @str: the string to duplicate
  *
  * Return: pointer to the duplicated string
