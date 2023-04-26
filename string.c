e6e986a01e75092e96245a88fbdd52f0e4a60d77
rafa906 <rafaibrahim1234@gmail.com>
2023-04-26 16:01:37 -0700

    string.c

--- string.c
+++ string.c
@@ -20,8 +20,8 @@ int _strlen(char *s)
 
 /**
  * _strcmp - perform lexicogarphic comparison of two strangs.
- * @s1: the 1st strang
- * @s2: the 2nd strang
+ * @s1: the first strang
+ * @s2: the second strang
  *
  * Return: negative if s1 < s2, positive if s1 > s2, zero if s1 == s2
  */
